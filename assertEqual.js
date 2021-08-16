2e9de1818b3c92908cbd8a5206a563a992b46113
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-16 18:55:19 +0000

    assertEqual implemnetation

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let result = '';
+  if (actual === expected)  {
+    result = console.log(`😀😀😀 ${actual} === ${expected}`);
+  } else  {
+    result = console.log(`😔😔😔 ${actual} !== ${expected}`);
+  }
+  return result;
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
