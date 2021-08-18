31883398771d986e2b9eebc52f1857e8fcc29de2
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-18 20:51:41 +0000

    w1d3

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  let result = '';
+  if (actual === expected)  {
+    result = console.log(`😀😀😀 ${actual} === ${expected}`);
+  } else  {
+    result = console.log(`😔😔😔 ${actual} !== ${expected}`);
+  }
+  return result;
+};
+
+const findKeyByValue = (obj, value) => {
+  const keys = Object.keys (obj);
+
+  for (const key of keys) {
+    if (obj[key] === value) return key;
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
