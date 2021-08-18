af4f3a2c2e4806269c38613245d44d14b724c663
Sara Sabet <sara.ilbeigi@gmail.com>
2021-08-18 13:47:42 +0000

    change the code for middle

--- middle.js
+++ middle.js
@@ -23,23 +23,23 @@ const assertArrayEqual = (arr1, arr2) => {
 const middle = function(array) {
   let arrLength = array.length;
   let middleNum = [];
-  if (arrLength < 2) {
+  let middArray = arrLength / 2;
+
+  if (arrLength < 2) {// if the array is empty or has one item
     return array;
   }
-  if (arrLength % 2 !== 0) {
-    middleNum.push(array[Math.floor(arrLength / 2)])
-    return ('odd', middleNum);
-  }
-  middleNum.push(array[(arrLength / 2) - 1])
-  middleNum.push(array[arrLength / 2])
 
-  return middleNum;
+  if (arrLength % 2 !== 0) {// if the array length is odd/3/7/9
+    return (array.slice(Math.floor(middArray), Math.floor(middArray) + 1));
+  }
+
+  return array.slice( middArray-1, middArray+1); // if teh array length is even 2/4/6/10
 
 }
-console.log(middle([1, 2]))
-console.log(middle([1, 2,5,6,7]))
-console.log(middle([1]))
-console.log(middle([]))
-assertEqual(assertArrayEqual(middle([1,2]),[1,2]), true)
-assertEqual(assertArrayEqual(middle([1,2,3]),[2]), true)
-assertEqual(assertArrayEqual(middle([1]),[1]), false)
+console.log(middle([1, 2]))//1,2
+console.log(middle([1, 2,5,6,7]))//5
+console.log(middle([1]))//1
+console.log(middle([]))//[]
+assertEqual(assertArrayEqual(middle([1,2]),[1,2]), true)// true
+assertEqual(assertArrayEqual(middle([1,2,3]),[2]), true)// true
+assertEqual(assertArrayEqual(middle([1]),[1]), false)// false
